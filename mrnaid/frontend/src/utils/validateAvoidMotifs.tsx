dd3d9622897257e3c7c4bcb701ba532379cb2aa4
David Prihoda <david.prihoda1@merck.com>
2024-01-26 14:35:28 +0100

    Fix warning message

--- mrnaid/frontend/src/utils/validateAvoidMotifs.tsx
+++ mrnaid/frontend/src/utils/validateAvoidMotifs.tsx
@@ -12,7 +12,7 @@ export const validateAvoidMotifs = (rule: string, values: string[], callback: (e
   const customInputs = values.filter((value: string) => !motifs.includes(value))
   const invalidInputs = customInputs.filter((value: string) => !value.match(customMotifsRegex))
   if (invalidInputs.length !== 0) {
-    callback(new Error('Motif not found, please pick from the list, or provide a RNA sequence directly (ACGT/U)'))
+    callback(new Error('Motif not found, please pick from the list, or provide an RNA sequence directly (ACGT/U)'))
   } else {
     callback()
   }
